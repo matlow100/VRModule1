799400a27a8e0287f1a61aabad1384814349f613
Matt Lowe <matlow100@yahoo.com>
2021-02-10 18:55:38 -0600

    Fixed script errors

--- Assets/Scripts/ArrowTrigger.cs
+++ Assets/Scripts/ArrowTrigger.cs
@@ -1,16 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ArrowTrigger : MonoBehaviour
 {
+    public Text countText;
+    public Text winText;
+    
+    private Rigidbody rb;
+    private int count;
+    
+    void Start ()
+    {
+        // rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText ();
+        winText.text = "";
+    }
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Arrow"))
+        if (other.gameObject.CompareTag("projectile"))
         {
             other.gameObject.SetActive(false);
             count = count + 1;
             SetCountText();
+        }
+    }
+    
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString ();
+        if (count >= 10)
+        {
+            winText.text = "You Win!";
         }
     }
 }
